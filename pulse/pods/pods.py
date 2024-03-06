fd6bb446da620d08b4932855552b2649d2b41bef
Unkovic7 <stefan.unkovic2003@gmail.com>
2024-03-06 20:47:59 +0100

    Moved and renamed install functions

--- pulse/pods/pods.py
+++ pulse/pods/pods.py
@@ -1,11 +1,7 @@
 import click
 import os
-import pulse.core.git.git as git
-from ..get.get import get_download
-from pulse.core.core_dir import COMPILER_PATH, RUNTIME_PATH
 import shutil
-compilers_dict: dict[int, str] = {}
-runtimes_dict: dict[int, str] = {}
+import pulse.download.download as download
 
 
 @click.command
@@ -40,75 +36,18 @@ def pods() -> None:
             choice_mod = click.prompt("Enter your choice", type=click.IntRange(1, 2))
             if choice_mod == 1:
                 shutil.rmtree(os.path.join(os.getcwd(), ".pods/compiler"))
-                install_compiler()
+                download.get_compiler()
 
             if choice_mod == 2:
                 shutil.rmtree(os.path.join(os.getcwd(), ".pods/runtime"))
-                install_runtime()
+                download.get_runtime()
 
             return click.echo("Pulse pods has been successfully modified!")
 
         if choice == 3:
             return click.echo("Canceled!")
 
-    install_compiler_and_runtime()
+    download.get_compiler()
+    download.get_runtime()
     return click.echo("Pulse pods has been successfully initialized!")
 
-
-def install_compiler() -> None:
-    """
-    Prompts user to install specified compiler
-
-    Returns:
-        None
-    """
-    click.echo("Select the version of the compiler you would like to install.")
-    for i, release in enumerate(git.get_github_compiler_releases(), start=1):
-        compilers_dict[i] = release['name']
-        click.echo(f"{i}. {release['name']}")
-
-    compiler_choice = click.prompt("Enter your choice", type=click.IntRange(1, i))
-    compiler_path = os.path.join(COMPILER_PATH, compilers_dict[compiler_choice])
-    if not os.path.exists(compiler_path):
-        click.echo("Compiler is not found in the cache, it will be downloaded.")
-        click.echo(f"Downloading compiler ({compilers_dict[compiler_choice]})..")
-        get_download("compiler", compilers_dict[compiler_choice])
-
-    pods_compiler = os.path.join(os.getcwd(), ".pods/compiler")
-    shutil.copytree(os.path.join(COMPILER_PATH, compilers_dict[compiler_choice]), pods_compiler)
-
-def install_runtime() -> None:
-    """
-    Prompts user to install specified runtime
-
-    Returns:
-        None
-    """
-    click.echo("Select the version of the runtime you would like to install.")
-    for i, release in enumerate(git.get_github_runtime_releases(), start=1):
-        runtimes_dict[i] = release['name']
-        click.echo(f"{i}. {release['name']}")
-
-    runtime_choice = click.prompt("Enter your choice", type=click.IntRange(1, i))
-    continue_confirm = click.confirm("Do you want to continue?")
-    if not continue_confirm:
-        return click.echo("Cancelled.")
-
-    runtime_path = os.path.join(RUNTIME_PATH, runtimes_dict[runtime_choice])
-    if not os.path.exists(runtime_path):
-        click.echo("Runtime is not found in the cache, it will be downloaded.")
-        click.echo(f"Downloading runtime ({runtimes_dict[runtime_choice]})..")
-        get_download("runtime", runtimes_dict[runtime_choice])
-
-    pods_runtime = os.path.join(os.getcwd(), ".pods/runtime")
-    shutil.copytree(os.path.join(RUNTIME_PATH, runtimes_dict[runtime_choice]), pods_runtime)
-
-def install_compiler_and_runtime() -> None:
-    """
-    Starts installation of the compilers and the runtimes
-
-    Returns:
-        None
-    """
-    install_compiler()
-    install_runtime()
