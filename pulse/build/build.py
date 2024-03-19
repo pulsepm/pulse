30cac389e2a30554a27d8bd70ad05f4c991bc30e
Unkovic7 <stefan.unkovic2003@gmail.com>
2024-03-20 00:27:06 +0100

    Additional checks for invalid keys

--- pulse/build/build.py
+++ pulse/build/build.py
@@ -6,11 +6,12 @@ import subprocess
 from .build_compile import compile
 
 @click.command
-def build():
+@click.argument("mode", default="__global__mode__")
+def build(mode: str):
     data = {}
 
     if not os.path.exists(os.path.join(os.getcwd(), 'pulse.toml')):
-        print('This is not pulse package.')
+        click.echo('This is not pulse package.')
         return
     # pulse toml exists, read it
     with(open('pulse.toml', 'r')) as toml_file:
@@ -20,8 +21,33 @@ def build():
     print(data) # check the data
     project_data = data['project']
 
+    if not 'compiler' in data:
+        click.echo("You have to specify compiler options using [compiler] table.")
+        return 
+
     compiler_data = data['compiler']
-    print(compiler_data)
 
     # read the compiler data
-    compile(project_data['entry'], project_data['output'], compiler_data['version'], None if not 'options' in compiler_data else compiler_data['options'], None if not 'modules' in compiler_data else compiler_data['modules'], None if not 'legacy' in compiler_data else compiler_data['legacy'])
+    if mode == "__global__mode__":
+        if not 'version' in compiler_data:
+            click.echo("You don't have compiler version specified in your pulse.toml. Please specify it via `version = (version)` key, within compiler table or your compiler profile table.")
+            return
+
+        compile(project_data['entry'], project_data['output'], compiler_data['version'], None if not 'options' in compiler_data else compiler_data['options'], None if not 'modules' in compiler_data else compiler_data['modules'], None if not 'legacy' in compiler_data else compiler_data['legacy'])
+
+    else:
+        if not 'profiles' in compiler_data:
+            click.echo("You don't have profiles list.")
+            return
+
+        if not mode in compiler_data['profiles']:
+            click.secho('Unexisting profile selected.', fg='red')
+            return
+
+        profile_data = compiler_data['profiles'][f'{mode}']
+
+        if not 'version' in profile_data:
+            click.echo("You don't have compiler version specified in your pulse.toml. Please specify it via `version = (version)` key, within compiler table or your compiler profile table.")
+            return
+
+        compile(project_data['entry'], project_data['output'], profile_data['version'], None if not 'options' in profile_data else profile_data['options'], None if not 'modules' in profile_data else profile_data['modules'], None if not 'legacy' in profile_data else profile_data['legacy'])
