cf2b56f0d8be5f63dd22f44bf54a0ffd629e8702
Ykpauneu <mestodan230@gmail.com>
2024-03-25 22:46:45 +0300

    black & flake8 fixes

--- pulse/ensure/ensure.py
+++ pulse/ensure/ensure.py
@@ -32,22 +32,32 @@ def ensure() -> None:
             re_package[2]
         except:
             re_package.append(git_get.default_branch(re_package[0], re_package[1]))
-            click.echo("No tag, commit, or branch was specified in the requirements. The default branch name will be used!")
+            click.echo(
+                "No tag, commit, or branch was specified in the requirements. The default branch name will be used!"
+            )
 
         package_path = os.path.join(
             PACKAGE_PATH, f"{re_package[0]}/{re_package[1]}/{re_package[2]}"
         )
         if not os.path.exists(package_path):
-            click.echo(f"Package {re_package[0]}/{re_package[1]}/{re_package[2]} was not found, it will be installed.")
-            git_download.download_package(re_package[0], re_package[1], package_path, re_package[2])
+            click.echo(
+                f"Package {re_package[0]}/{re_package[1]}/{re_package[2]} was not found, it will be installed."
+            )
+            git_download.download_package(
+                re_package[0], re_package[1], package_path, re_package[2]
+            )
 
         tmp_dir = os.path.join(REQUIREMENTS_PATH, f"{re_package[1]}")
         if not os.path.exists(tmp_dir):
             os.makedirs(tmp_dir)
             is_plugin: bool = False
-            plugins_path = os.path.join(PLUGINS_PATH, f"{re_package[0]}/{re_package[1]}")
+            plugins_path = os.path.join(
+                PLUGINS_PATH, f"{re_package[0]}/{re_package[1]}"
+            )
             for file in os.listdir(plugins_path):
-                if file.endswith(f"{'.dll' if platform.system() == 'Windows' else '.so'}"):
+                if file.endswith(
+                    f"{'.dll' if platform.system() == 'Windows' else '.so'}"
+                ):
                     click.echo(f"Found plugin: {file}!")
                     shutil.copy2(os.path.join(plugins_path, file), tmp_dir)
                     is_plugin = True
@@ -59,4 +69,6 @@ def ensure() -> None:
         else:
             click.echo("The requirement already exists..")
 
-        print(f"Package {re_package[0]}/{re_package[1]} ({re_package[2]}) has been successfully migrated!")
+        print(
+            f"Package {re_package[0]}/{re_package[1]} ({re_package[2]}) has been successfully migrated!"
+        )
