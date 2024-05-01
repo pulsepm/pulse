787cec4faee008fa74207b56adccbb4a97a8f867
Unkovic7 <stefan.unkovic2003@gmail.com>
2024-05-01 23:41:20 +0200

    Scrolls theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,6 @@ extensions = [
     'sphinx_rtd_theme',
 ]
 
-html_theme = "sphinx_rtd_theme"
-
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -27,5 +25,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'scrolls'
 html_static_path = ['_static']
