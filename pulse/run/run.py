5689e8256eb18e56d42b4163e0f1e1a43a46c8c4
Mergevos <stefan.unkovic2003@gmail.com>
2024-06-05 16:53:11 +0200

    Stroke the run module.

--- pulse/run/run.py
+++ pulse/run/run.py
@@ -6,7 +6,9 @@ import shutil
 import click
 import tomli
 import json
+import logging
 
+import pulse.stroke.stroke as stroke
 import pulse.download.download as download
 from pulse.core.core_dir import RUNTIME_PATH, PODS_PATH, REQUIREMENTS_PATH
 from pulse.package.package_ensure import ensure_packages
@@ -22,11 +24,13 @@ def run(ensure: bool) -> None:
 
     # read the version
     if not os.path.exists(os.path.join(os.getcwd(), "pulse.toml")):
-        print("This is not a pulse package!")
+        logging.fatal("Fatal error occurred -> Not a valid Pulse package. Exit code: 2")
+        stroke.dump(2)
         return
 
     # Ensure all plugins are there (just run ensure)
     # read the toml
+    logging.debug("Gathering information...")
     data = {}
     json_data = {}
     pods = os.path.exists(PODS_PATH) and os.path.isdir(PODS_PATH)
@@ -34,48 +38,62 @@ def run(ensure: bool) -> None:
     runtime_plugins = os.path.join(PODS_PATH, 'runtime', 'plugins') if pods else os.path.join(RUNTIME_PATH, data['runtime']['version'], "plugins")
     runtime_loc = os.path.join(RUNTIME_PATH, data['runtime']['version']) if not pods else os.path.join(PODS_PATH, "runtime")
 
+    logging.debug("Informations gathered, loading from files...")
+
     with open("pulse.toml", 'rb') as toml_config:
         data = tomli.load(toml_config)
 
+    # This should be built on pulse.toml
     with open(os.path.join(runtime_loc, "config.json"), 'r') as json_file:
         json_data = json.load(json_file)
+
+    logging.info("Files has been parsed succesfully!")
 
     if ensure:
         ensure_packages()
 
+    logging.debug("Determinating OS...")
     system = platform.system()
     file_name = os.path.join(
         os.path.join(PODS_PATH, "runtime") if pods else os.path.join(RUNTIME_PATH, data['runtime']['version']), "omp-server.exe" if system == "Windows" else "omp-server"
     )
 
+    logging.debug("OS determinated. Checking for runtime...")
+
     # if pods, just run the server from
     if not os.path.exists(file_name) and not pods:
         # read the toml
         if 'version' in data['runtime']:
-            click.echo("There's no downloaded runtimes. Downloading the specified one...")
+            logging.warning("There's no downloaded runtimes. Downloading the specified one...")
             download.get_asset('runtime', data['runtime']['version'])
 
         else:
-            click.echo("No specified version") # Fatal here
+            logging.fatal("Fatal error occurred -> Error cloning repository. Exit code: 31")
+            stroke.dump(31)
             return
 
     # move plugins and add them to config.json
     # plugins should be moved to ppc/runtime/version/plugins and added to the respective config.json through pulse.toml or in .pods if pods
     # now loop through plugins
+    logging.debug("Moving plugins and adding the to config.json.")
     json_data['pawn']['legacy_plugins'].clear()
 
     if os.path.exists(os.path.join(REQUIREMENTS_PATH, "plugins")):
         for file in os.listdir(os.path.join(REQUIREMENTS_PATH, "plugins")):
             full_file = os.path.join(REQUIREMENTS_PATH, "plugins", file)
-            print(full_file)
             if os.path.isfile(full_file):
                 os.makedirs(runtime_plugins, exist_ok=True)
 
                 shutil.copy(full_file, runtime_plugins)
                 # add them to config.json
                 json_data['pawn']['legacy_plugins'].append(file)
+                logging.debug(f"Appendend file: {file}")
+    
+    logging.info("Plugins has been moved succesfully.")
+    
 
     #move the mode
+    logging.debug("Moving the gamemode and setting it to config.json...")
     shutil.copy(os.path.join(os.getcwd(), data['project']['output']), os.path.join(runtime_loc, "gamemodes"))
 
     with open(os.path.join(runtime_loc, "config.json"), 'w') as json_file:
--- pulse/run/run_server.py
+++ pulse/run/run_server.py
@@ -1,15 +1,13 @@
 import subprocess
+import logging
 
 def server(file_name: str) -> int:
-    try:
-        if '.pods' in file_name:
-            print(f"Running the open.mp server with pods!")
-        else:
-            print(f"Running the open.mp server with pulse!")
+    if '.pods' in file_name:
+        logging.info(f"Running the open.mp server with pods...")
+    else:
+        logging.info(f"Running the open.mp server with pulse...")
 
-        server_arguments = ['-c', 'config.json']
-        server_command = [file_name] + server_arguments
-        
-        subprocess.run(server_command)
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
+    server_arguments = ['-c', 'config.json']
+    server_command = [file_name] + server_arguments
+    
+    subprocess.run(server_command)
