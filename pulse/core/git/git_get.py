960bc71304ddd68799efbd5846cb0b5f3f964394
Mergevos <stefan.unkovic2003@gmail.com>
2024-07-23 01:58:09 +0200

    Get repo field and get includes

--- pulse/core/git/git_get.py
+++ pulse/core/git/git_get.py
@@ -144,3 +144,40 @@ def get_resource_plugins(directory, package_type: Literal["pulse", "sampctl"]) -
 
     except:
         return None
+
+
+def get_resource_includes(directory, package_type: Literal["pulse", "sampctl"]) -> list[str] | None:
+    try:
+        with open(os.path.join(directory, "pulse.toml" if package_type == "pulse" else "pawn.json"), mode="rb" if package_type == "pulse" else "r") as f:
+            if package_type == "pulse":
+                resource = tomli.load(f)
+            else:
+                resource = json.load(f)
+
+        if package_type == "pulse":
+            return resource["resource"][system().lower()]["includes"]
+
+        else:
+            index: int = 0 if resource["resources"][0]["platform"] == system().lower() else 1
+            return resource["resources"][index]["includes"]
+
+    except:
+        return None
+
+
+def get_resource_repo(directory, package_type: Literal["pulse", "sampctl"]) -> str | None:
+    try:
+        with open(os.path.join(directory, "pulse.toml" if package_type == "pulse" else "pawn.json"), mode="rb" if package_type == "pulse" else "r") as f:
+            if package_type == "pulse":
+                resource = tomli.load(f)
+            else:
+                resource = json.load(f)
+
+        if package_type == "pulse":
+            return resource["project"]["repo"]
+
+        else:
+            return resource["repo"]
+
+    except:
+        return None 
