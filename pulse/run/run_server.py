431f81e28fa4d8eb9af4153ea1d32a66ae9aedc6
Mergevos <stefan.unkovic2003@gmail.com>
2024-12-27 15:21:56 +0100

    Run code neating

--- pulse/run/run_server.py
+++ pulse/run/run_server.py
@@ -1,13 +1,20 @@
 import subprocess
 import logging
 
-def server(file_name: str) -> int:
-    if '.pods' in file_name:
-        logging.info(f"Running the open.mp server with pods...")
-    else:
-        logging.info(f"Running the open.mp server with pulse...")
 
-    server_arguments = ['-c', 'config.json']
+def server(file_name: str) -> int:
+    """
+    Runs open.mp server with specified config file.
+
+    Args:
+        file_name (str): Configuration file name.
+    """
+    if ".pods" in file_name:
+        logging.info("Running the open.mp server with pods...")
+    else:
+        logging.info("Running the open.mp server with pulse...")
+
+    server_arguments = ["-c", "config.json"]
     server_command = [file_name] + server_arguments
-    
+
     subprocess.run(server_command)
